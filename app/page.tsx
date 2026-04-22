938093fb234c813a310479541e0109b41590dfc0
Prakash <prakashsingha@gmail.com>
2026-04-22 17:26:42 +0800

    Update cache

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { cacheLife } from "next/cache";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const page = async () => {
   "use cache";
-  cacheLife({ revalidate: 60 * 60 }); // Cache for 1 hours
+  cacheLife("hours");
   const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
 
